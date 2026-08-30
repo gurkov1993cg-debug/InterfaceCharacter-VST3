eaf0d5cfb126c35552de011c3aa013ea7dddf001
gurkov1993cg-debug <gurkov.1993.cg@gmail.com>
2026-08-31 01:40:30 +0300

    Add src/vst3/PluginIds.hpp

---
+++ src/vst3/PluginIds.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "pluginterfaces/base/funknown.h"
+
+namespace interface_character::vst3 {
+
+static const Steinberg::FUID kProcessorUid (0x5E7C2A11, 0x2F1C4D8A, 0xA16E90C3, 0x7B4D2F08);
+static const Steinberg::FUID kControllerUid (0xA90D3F62, 0x6C8B47E1, 0xB52A19D4, 0xE7038C5F);
+
+} // namespace interface_character::vst3
